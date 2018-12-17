808ebb9adf98c9706932466d2a2e4181ab9830dd
Northrend <286424061@qq.com>
2018-12-17 16:12:50 +0800

    rename mxnet glcv scripts

--- mxnet-cubicle/img-cls/train-gluon/glcv_export_symbol.py
+++ mxnet-cubicle/img-cls/train-gluon/glcv_export_symbol.py
@@ -1,5 +1,8 @@
 import mxnet
 import mxnet.gluon.model_zoo.vision.resnet as res
 
-net = a = res.resnet50_v2()
+net = res.resnet50_v2()
+net(mxnet.sym.Variable("data")).save("resnet50_v2-symbol.json")
+
+net = res.resnet50_v2()
 net(mxnet.sym.Variable("data")).save("resnet50_v2-symbol.json")
